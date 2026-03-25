9dc40faea53713aa8eec57f68c908a27adc98c29
Your Name <you@example.com>
2026-03-24 18:39:46 -0700

    Update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
 
       <Routes>
-
+        <Route path="/" element={<Navigate to="/register" replace />} />
         <Route path="/register" element={<RegisterPage />} />
 
       </Routes>
